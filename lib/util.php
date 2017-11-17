1bc8d6f67b91f346b2baeb711ac057fbda001b04
TekkCraft <marc.wilhelm.ch@gmail.com>
2017-11-17 15:27:30 +0100

    Removed services.php

--- lib/util.php
+++ lib/util.php
@@ -17,3 +17,19 @@ function app(): App
     }
     return $app;
 }
+
+/**
+ * Handling email
+ *
+ * This function is shortening for filter_var.
+ *
+ * @see filter_var()
+ *
+ * @param string $email to check
+ *
+ * @return mixed
+ */
+function is_email($email)
+{
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
