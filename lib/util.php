8771ed993792b95ba473c9f8d9be14a9f4becbc3
TekkCraft <marc.wilhelm.ch@gmail.com>
2017-11-17 15:28:09 +0100

    Clean up util.php

--- lib/util.php
+++ lib/util.php
@@ -11,8 +11,6 @@ function app(): App
 {
     static $app = null;
     if ($app === null) {
-        //$config = require_once __DIR__ . '/../config/config.php';
-        //$app = new App($config);
         $app = new App(['settings' => require __DIR__ . '/../config/config.php']);
     }
     return $app;
