5db2118e10780fb18d149356a8d249b32b09595b
TekkCraft <marc.wilhelm.ch@gmail.com>
2017-11-24 16:11:45 +0100

    Added missing doc blocks

--- src/Adapter/MailgunAdapter.php
+++ src/Adapter/MailgunAdapter.php
@@ -16,12 +16,27 @@ class MailgunAdapter
      */
     protected $domain;
 
+    /**
+     * MailgunAdapter constructor.
+     *
+     * @param string $domain Domain
+     * @param Mailgun $mailgun Mailgun
+     */
     public function __construct($domain, Mailgun $mailgun)
     {
         $this->mailgun = $mailgun;
         $this->domain = $domain;
     }
 
+    /**
+     * Send mail
+     *
+     * @param string $from Sender
+     * @param string $to Receiver
+     * @param string $subject Mail subject
+     * @param string $text Mail text
+     * @return void
+     */
     public function sendMail($from, $to, $subject, $text)
     {
         $this->mailgun->messages()->send($this->domain, [
