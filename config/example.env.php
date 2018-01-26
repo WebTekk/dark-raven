c43d31f44cbdc0b351ee2aa42907d1cae599b0cb
TekkCraft <marc.wilhelm.ch@gmail.com>
2018-01-26 10:45:11 +0100

    Updated env.example.php

--- config/example.env.php
+++ config/example.env.php
@@ -8,5 +8,7 @@ $env['db']['username'] = "username";
 $env['db']['password'] = "password";
 
 $env['twig']['debug'] = true;
+$env['twig']['autoReload'] = true;
+$env['twig']['assetCache']['cache_enabled'] = false;
 
 return $env;
