97074843741fc0c34eafbf146ec871ef558a96bf
TekkCraft <marc.wilhelm.ch@gmail.com>
2018-03-08 11:39:18 +0100

    Fixed js and css coding style

--- templates/Layout/layout.js
+++ templates/Layout/layout.js
@@ -1,41 +1,41 @@
 var Layout = function () {
 
-	var $this = this;
+    var $this = this;
 
-	/**
-	 * Layout constructor
-	 */
-	this.constructor = function () {
-		this.registerEvents();
-	};
+    /**
+     * Layout constructor
+     */
+    this.constructor = function () {
+        this.registerEvents();
+    };
 
-	this.registerEvents = function () {
-		$("#navbar-collapse").find('[data-id=logout_link]').on('click', this.logoutOnPress);
-	};
+    this.registerEvents = function () {
+        $("#navbar-collapse").find('[data-id=logout_link]').on('click', this.logoutOnPress);
+    };
 
-	this.logoutOnPress = function () {
-		showLoader();
-		var url = baseurl() + "/logout";
-		$.ajax({
-			type: "GET",
-			contentType: "application/json",
-			url: url,
-			cache: false
-		}).done(function (json) {
-			var data = JSON.parse(json);
-			if (data.logout === true) {
-				hideLoader();
-				$(location).attr("href", baseurl());
-			}
-		});
-	};
+    this.logoutOnPress = function () {
+        showLoader();
+        var url = baseurl() + "/logout";
+        $.ajax({
+            type: "GET",
+            contentType: "application/json",
+            url: url,
+            cache: false
+        }).done(function (json) {
+            var data = JSON.parse(json);
+            if(data.logout === true) {
+                hideLoader();
+                $( location ).attr("href", baseurl());
+            }
+        });
+    };
 
-	this.constructor();
+    this.constructor();
 };
 
 /**
  * Start JavaScript when document is ready.
  */
 $(function () {
-	new Layout();
+    new Layout();
 });
